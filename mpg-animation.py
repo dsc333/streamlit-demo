8063a3ae6bd92e29459333a2d9c2c5080dde2200
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2026-04-21 13:26:26 -0400

    Add files via upload

---
+++ mpg-animation.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/refs/heads/main/mpg.csv')
+df.dropna(inplace=True)
+
+st.header('Fuel efficiency (1970-1982)')
+fig = px.scatter(df,
+            x="mpg",
+            y="horsepower",
+            animation_frame="model_year",
+            size="mpg",
+            color="origin",
+            hover_name="name",
+            size_max=30,
+            range_x=[5,50],
+            range_y=[0,300])
+
+event = st.plotly_chart(fig, key="mpg", on_select="rerun")
+
+st.header('Data source')
+st.dataframe(df)
